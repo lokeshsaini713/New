f55df622c8573d91b2166b5d57546edb26b52710
lokeshsaini713 <49331826+lokeshsaini713@users.noreply.github.com>
2020-02-14 07:04:31 +0530

    Add files via upload

---
+++ testgridview.aspx
@@ -0,0 +1,47 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="testgridview.aspx.cs" Inherits="testgridview" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+
+            <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="false" DataKeyNames="id" OnPageIndexChanging="GridView1_PageIndexChanging" OnRowCancelingEdit="GridView1_RowCancelingEdit" OnRowDeleting="GridView1_RowDeleting" OnRowEditing="GridView1_RowEditing" OnRowUpdating="GridView1_RowUpdating">
+                <Columns>
+
+                    <asp:TemplateField HeaderText="Membership No.">
+                                                <ItemStyle HorizontalAlign="Center" />
+                                                <FooterStyle HorizontalAlign="Center" />
+                                                <ItemTemplate>
+                                                    <asp:Literal Text='<%#Eval("membershipno").ToString().ToUpper()%>' ID="MEMBERSHIPNUMBER" runat="server" />  
+                                                
+                                                </ItemTemplate>
+
+                        <EditItemTemplate>
+                <asp:DropDownList ID="ddlCities" runat="server">
+                    <asp:ListItem Text="text1" />
+                    <asp:ListItem Text="text2" />
+                </asp:DropDownList>
+            </EditItemTemplate>
+
+                                            </asp:TemplateField>
+
+
+                    <asp:BoundField DataField="id" HeaderText="S.No." />
+                    <asp:BoundField DataField="name" HeaderText="Name" />
+                    <asp:BoundField DataField="address1" HeaderText="address" />
+                    <asp:BoundField DataField="country" HeaderText="Country" />
+                    <asp:CommandField ShowEditButton="true" />
+                    <asp:CommandField ShowDeleteButton="true" />
+
+                </Columns>
+            </asp:GridView>
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ testgridview.aspx.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class testgridview : System.Web.UI.Page
+{
+    private SqlConnection conn = new SqlConnection("Data Source=.;Integrated Security=true;Initial Catalog=GJS2019");
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            gvbind();
+        }
+    }
+    protected void gvbind()
+    {
+        conn.Open();
+        SqlCommand cmd = new SqlCommand("Select * from visitor1", conn);
+        SqlDataAdapter da = new SqlDataAdapter(cmd);
+        DataSet ds = new DataSet();
+        da.Fill(ds);
+        conn.Close();
+        if (ds.Tables[0].Rows.Count > 0)
+        {
+            GridView1.DataSource = ds;
+            GridView1.DataBind();
+        }
+        else
+        {
+            ds.Tables[0].Rows.Add(ds.Tables[0].NewRow());
+            GridView1.DataSource = ds;
+            GridView1.DataBind();
+            int columncount = GridView1.Rows[0].Cells.Count;
+            GridView1.Rows[0].Cells.Clear();
+            GridView1.Rows[0].Cells.Add(new TableCell());
+            GridView1.Rows[0].Cells[0].ColumnSpan = columncount;
+            GridView1.Rows[0].Cells[0].Text = "No Records Found";
+        }
+    }
+    protected void GridView1_RowDeleting(object sender, GridViewDeleteEventArgs e)
+    {
+        GridViewRow row = (GridViewRow)GridView1.Rows[e.RowIndex];
+        Label lbldeleteid = (Label)row.FindControl("lblID");
+        conn.Open();
+        SqlCommand cmd = new SqlCommand("delete FROM detail where id='" + Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Value.ToString()) + "'", conn);
+        cmd.ExecuteNonQuery();
+        conn.Close();
+        gvbind();
+    }
+    protected void GridView1_RowEditing(object sender, GridViewEditEventArgs e)
+    {
+        GridView1.EditIndex = e.NewEditIndex;
+
+        //e.NewEditIndex
+
+        gvbind();
+    }
+    protected void GridView1_RowUpdating(object sender, GridViewUpdateEventArgs e)
+    {
+        int userid = Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Value.ToString());
+        GridViewRow row = (GridViewRow)GridView1.Rows[e.RowIndex];
+        Label lblID = (Label)row.FindControl("lblID");
+        //TextBox txtname=(TextBox)gr.cell[].control[];  
+        TextBox textName = (TextBox)row.Cells[0].Controls[0];
+        TextBox textadd = (TextBox)row.Cells[1].Controls[0];
+        TextBox textc = (TextBox)row.Cells[2].Controls[0];
+        //TextBox textadd = (TextBox)row.FindControl("txtadd");  
+        //TextBox textc = (TextBox)row.FindControl("txtc");  
+        GridView1.EditIndex = -1;
+        conn.Open();
+        //SqlCommand cmd = new SqlCommand("SELECT * FROM detail", conn);  
+        SqlCommand cmd = new SqlCommand("update detail set name='" + textName.Text + "',address='" + textadd.Text + "',country='" + textc.Text + "'where id='" + userid + "'", conn);
+        cmd.ExecuteNonQuery();
+        conn.Close();
+        gvbind();
+        //GridView1.DataBind();  
+    }
+    protected void GridView1_PageIndexChanging(object sender, GridViewPageEventArgs e)
+    {
+        GridView1.PageIndex = e.NewPageIndex;
+        gvbind();
+    }
+    protected void GridView1_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+    {
+        GridView1.EditIndex = -1;
+        gvbind();
+    }
+}
